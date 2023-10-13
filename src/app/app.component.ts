14d05df8eee341f3f3e479b87e65fad6ee6e61e2
Anas <anas.kala@hotmail.com>
2023-10-13 19:52:21 +0200

    extracting attributes from the response

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,18 @@ import { WeatherApiService } from './weather-api.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  headline: string = '';
-  severity: string = '';
-  event: string = '';
+  country: string = '';
+  localtime: string = '';
+  tz_id: string = '';
 
   constructor(private weatherApiService: WeatherApiService) {
     this.weatherApiService.getWeather().subscribe((response) => {
       console.log('This is the response');
-      console.log(response);
+      console.log(response.location);
+      this.country=response.location.country;
+      this.localtime=response.location.localtime;
+      this.tz_id=response.location.tz_id;
+      //this.country=response.location.country;
     });
   }
 }
--- src/app/weather-api.service.ts
+++ src/app/weather-api.service.ts
@@ -2,12 +2,11 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 interface WeatherResponsing {
-  alerts: {
-    alert: {
-      headline: string;
-      severity: string;
-      event: string;
-    }[];
+
+  location: {
+    country: string,
+    localtime: string,
+    tz_id: string
   }
 }
 
@@ -18,16 +17,15 @@ interface WeatherResponsing {
 export class WeatherApiService {
 
   private apiKey = '585a9a7c6fe0472fb21155407231310';
-  private apiUrl = 'https://api.weatherapi.com/v1/current.json';
+  private apiUrl = 'https://api.weatherapi.com/v1/timezone.json';
 
   constructor(private http: HttpClient) { }
 
   getWeather() {
-    return this.http.get<WeatherResponsing>(`${this.apiUrl}?key=${this.apiKey}&q=${location}`, {
+    return this.http.get<WeatherResponsing>(`${this.apiUrl}?q=${location}&lang=en`, {
       headers: {
         key: `${this.apiKey}`,
-        lang: 'de',
-        q: `${location}`
+        alerts: 'yes'
       }
     });
   }
